6e609a61987907098e272e30b28e0e1e7f6f4e7c
Dzulfikar Dzulfikar <kardzulfi1805@gmail.com>
2022-03-06 21:12:31 +0700

    added gloval filters and pipes, aswell add lines to enable DI for custom class validator rule

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
+import { CastErrorExceptionFilter } from './filters/cast-error-exception.filter';
+import { GeneralSuccessExceptionFilter } from './filters/general-success-exception.filter';
+import { InternalServerErrorExceptionFilter } from './filters/internal-server-error-exception.filter';
+import { MongoExceptionFilter } from './filters/mongo-validation-exception.filter';
+import { NotFoundExceptionFilter } from './filters/not-found-exception.filter';
+import { RouteNotFoundExceptionFilter } from './filters/route-not-found-exception.filter';
+import { UnprocessableEntityExceptionFilter } from './filters/unprocessable-entity.filter';
+import { ValidationExceptionPipe } from './pipes/validation-exception.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // DI in custom class validator
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  //global filters exception
+  app.useGlobalFilters(new RouteNotFoundExceptionFilter());
+  app.useGlobalFilters(new InternalServerErrorExceptionFilter());
+
+  // global failed validation exception
+  app.useGlobalPipes(new ValidationExceptionPipe());
+
   await app.listen(3000);
+  console.log(`Server is running on http://localhost:3000`)
 }
 bootstrap();
